1c0c75066aff002b3aed152f1500dd6d27e91d6b
Aliaksandr Butsin <aliaksandrbutsin@gmail.com>
2023-04-14 17:07:00 +0200

    add: solution

--- src/what-season.js
+++ src/what-season.js
@@ -12,16 +12,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  if (!date) return 'Unable to determine the time of year!';
-  if (!(date instanceof Date) || isNaN(date.getTime())) throw new Error('Invalid date!');
-  const year = date.getFullYear();
-  // if (year > 2100 && date.getMonth() === 0 && date.getDate() === 1 && date.getHours() === 0 && date.getMinutes() === 0 && date.getSeconds() === 0 && date.getMilliseconds() === 0) {
-  //   throw new Error('Invalid date!');
-  // }
-  const month = date.getMonth();
-  return month < 2 || month === 11 ? 'winter' : month < 5 ? 'spring' : month < 8 ? 'summer' : 'autumn';
-}
+  
+    if(!date) return 'Unable to determine the time of year!';
+  
+    try {
+      date.getTime();
+    }
+    catch (error) {
+      throw new Error ('Invalid date!');
+    }
+    let month = date.getMonth();
+  
+    if(month <= 1 || month === 11) return 'winter'; 
+    if(month > 1 && month <= 4) return 'spring';
+    if(month > 4 && month <= 7) return 'summer';
+    if(month > 7 && month <= 10) return 'autumn'; 
+  }
+
 
 module.exports = {
   getSeason
 };
+
+// getSeason(new Date(1682, 0, 28, 4, 2, 55, 833));
